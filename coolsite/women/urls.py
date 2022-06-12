bf212706f50c90e4cd48ef200d1a401d5cede106
ImKhisam <imkhisam@gmail.com>
2022-06-12 20:21:54 +0300

    12.06.22 daily

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='mainpage'),
-    path('cats/<int:catid>/', categories),
+    path('', index, name='home'),
     path('about/', about, name='about'),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -4,25 +4,56 @@ from django.shortcuts import redirect
 
 from .models import *
 
-menu = ['About site', 'Add article', 'Contact us', 'Log in']
+menu = [{'title': "About site", 'url_name': 'about'},
+        {'title': "Add article", 'url_name': 'add_page'},
+        {'title': "Feedback", 'url_name': 'contact'},
+        {'title': "Log in", 'url_name': 'login'}
+]
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Main page'})
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'cats':cats,
+        'menu': menu,
+        'title': 'Main page',
+        'cat_selected':0,
+    }
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About site'})
 
-def categories(request, catid):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Articles by category</h1><p>{catid}</p>")
+def addpage(request):
+    return HttpResponse('Add page')
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('mainpage', permanent=True)
+def contact(request):
+    return HttpResponse('Feedback')
 
-    return HttpResponse(f"<h1>Archive by year</h1><p>{year}</p>")
+def login(request):
+    return HttpResponse('Authorization')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Showing page with id={post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Category filter',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
+
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound("<h1>There is no such page</h1>")
